23cdc227fc339574b81a2e1e207b2fb3328dbcf6
toddmoy <todd.moy@gmail.com>
2014-07-05 02:59:04 -0400

    Changes

--- config.rb
+++ config.rb
@@ -108,10 +108,10 @@ configure :build do
   # activate :asset_hash
 
   # Use relative URLs
-  # activate :relative_assets
+  activate :relative_assets
 
   # Or use a different image path
-  set :http_prefix, "training-and-recovery" # This should match the repo name if deploying to gh-pages
+  # set :http_prefix, "/training-and-recovery/"
 end
 
 activate :deploy do |deploy|
